20ee4f6fea5a406dff4b111d5bbec924301684b1
jckli <me@jackli.dev>
2025-11-30 23:24:58 +0000

    feat: add initial site

---
+++ site/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+	return (
+		<>
+			<div className="w-full h-[100vh] flex flex-col items-center justify-center text-white text-3xl font-semibold text-center">
+				<h1 className="mb-6">Groovee</h1>
+			</div>
+		</>
+	);
+}
